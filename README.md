afd2bd54f24496acf0525ee1a7ea343ccfa221d8
Peter Krauss <ppkrauss@gmail.com>
2019-12-29 11:01:53 -0300

    first draft

--- README.md
+++ README.md
@@ -1,18 +1,12 @@
 # S2geometry PoC
 
-Proof of Concept (PoC) for PostgreSQL implementation of [S2geometry](http://S2geometry.io) functions, based on [AfieldTrails/s2-postgis](https://github.com/AfieldTrails/s2-postgis) and [sidewalklabs/s2sphere](https://github.com/sidewalklabs/s2sphere). If our PoC applications evolve, we can invest in a real good S2geometry implementation for PostgreSQL, because both are C++, there are no need of Python, but there are a cost of hours of C++ programmers.
+Proof of Concept (PoC) for a PostgreSQL implementation of the [S2 Geometry Library](http://S2geometry.io), based on [AfieldTrails/s2-postgis](https://github.com/AfieldTrails/s2-postgis) and [sidewalklabs/s2sphere](https://github.com/sidewalklabs/s2sphere). This library not need PostGIS to run, but in general PostGIS will be used as "context" for geographical applications and library's test.
+<!-- S2 cell encoding using the python s2sphere library-->
+## Installation
 
-------
+This extension makes it easy to convert GIS data to S2 data types &mdash; S2 cell identifiers (S2cellID's) and S2cellID tokens in particular.
 
-## Original AfieldTrails/s2-postgis README
-
-s2 cell encoding using the python s2sphere library
-
-S2 Cell Id functions for PostgreSQL and PostGIS
-
-This extension makes it easy to convert GIS data to S2 data types - s2 cell ids and s2 cell id tokens in particular.
-
-To use, you must install plpython3u (the PostgreSQL language) and s2sphere (the python3 exstension) on your postgresql server.
+To use, you must [install plpython3u](https://www.enterprisedb.com/edb-docs/d/edb-postgres-advanced-server/installation-getting-started/installation-guide/9.6/EDB_Postgres_Advanced_Server_Installation_Guide.1.60.html) (the PostgreSQL PL/Phyton3 language) and S2Sphere (the python3 exstension) on your Postgresql server.
 
 ```
 sudo apt-get install postgresql-plpython3-9.6
@@ -40,8 +34,36 @@ S2 cell ids are 8-bytes, so fit nicely in PostgreSQL's `bigint`. Tokens are a he
 
 Either representation will work well for sorted indices. I generally prefer the `bigint` ids but try to provide functions for both when appropriate.
 
-If you're looking at the implementation, there is a dance around converting the unsigned representation used by python3 and s2sphere and the signed PostgreSQL implementation.
+If you're looking at the implementation, there is a dance around converting the unsigned representation used by Python3 and S2sphere and the signed PostgreSQL implementation.
+
+## s2-postgis differences
+Listing changes as doing commits:
+
+1. README, file names and formating SQL code.
+
+2. At functions remove all "STRICT" clauses because [it is a PostgreSQL bug](https://github.com/PostgREST/postgrest/issues/1294). To be readable at [PostgREST](https://postgrest.org), the comments are also transformed into SQL comments. Migrating also from public schema to s2gem schema, to simplify isolation (drop cascade only the lib) and review (not polluting public namespace).
+
+3. Add new functions: *s2_cellid_exact_area, s2_token_exact_area, s2_token_get_vertex, s2_token_edge_neighbors_array,s2_token_children_array*...
 
 ## Contributions
 
-Functions are added completely opportunistically, so feel free to request something or contribute! If we've missed some best practices or you want to convert it to use the c library instead of python, let me know!
+Functions are added completely opportunistically, so feel free to request something or contribute! If we've missed some best practices or you want to convert it to use the C library instead of python, let we know! Check also evolution of the [AfieldTrails/s2-postgis](https://github.com/AfieldTrails/s2-postgis) project.
+
+----
+
+## Why it is a PoC?
+
+This project will be never a "final project", it is a *proof of concept* because:
+
+1. A serious S2geometry implementation for PostgreSQL **not need a "Python intermediate"** module, both PostgreSQL and S2geometry are C++ sources... But C++ programmers are rare, we can't invest at this moment. If PoC applications evolve we can invest.
+
+2. We are evaluating best human-readable S2CellID representation, using alternatives for hexadecimal token, that **preserves hierarchy** and offer intermediate (half) levels:
+
+    *   base4 instead base16 representation (and transforming 3 bits *face* ID into 4 bits to preserve hierarchy).
+    *   testing the use of [**base4h** representation](http://osm.codes/_foundations/art1.pdf), to add "half levels" with its geometry expressed by a [*half level* **degenerated grid**](http://osm.codes/_foundations/art3.pdf).
+
+3. We are evaluating performance and usability of alternatives for PostgreSQL's *internal representation* of S2CellID: [bigint = int8](https://www.postgresql.org/docs/current/datatype-numeric.html#DATATYPE-INT) (most probable), [bitstring](https://www.postgresql.org/docs/current/datatype-bit.html), [bytea](https://www.postgresql.org/docs/current/datatype-binary.html) or [UUID](https://www.postgresql.org/docs/12/datatype-uuid.html).
+
+4. There are a plan to include in the same library all other utility functions that **enable base4h,  base16h and base32 hierarchical representations**,   and functions that enable express (split/merge) cells of the degenerated grid as union of ordinary cells.
+
+5. There are a plan extend library  by wrap functions and some new functions to enable "array algebra", "token albebra"  and another utilities. There are also plans to use the library in a [PostgREST interface](https://postgrest.org), with public serves at *OSM.codes*.
---
+++ s2gem-main.sql
@@ -0,0 +1,238 @@
+-- Requires plpython3u language to be installed
+-- CREATE LANGUAGE plpython3u;
+-- Also requires the s2sphere extension to be installed on the system for python3.
+
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION s2" to load this file. \quit
+-- that is: CREATE plpython3u and s2 EXTENSIONs before load this file
+
+
+--
+-- Conversion functions
+--
+
+-- PostgreSQL doesn't handle Unsigned Bigint, so we have to re-interpret the byte representation
+-- of the id.
+CREATE or replace FUNCTION s2_cellid_from_latlng(lat double precision, lng double precision) RETURNS bigint
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId.from_lat_lng(s2sphere.LatLng.from_degrees(lat, lng))
+  return int.from_bytes(id.id().to_bytes(8, 'big', signed=False), 'big', signed=True)
+$f$ LANGUAGE plpython3u IMMUTABLE;
+COMMENT ON function s2_cellid_from_latlng IS 'Convert from  latitude and longitude to cellid (long)';
+
+CREATE or replace FUNCTION s2_cellid_from_token(token text) RETURNS bigint
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  return int.from_bytes(id.id().to_bytes(8, 'big', signed=False), 'big', signed=True)
+$f$ LANGUAGE plpython3u IMMUTABLE;
+COMMENT ON function s2_cellid_from_token IS 'Convert from token (text) to cellid (long) representation';
+
+CREATE or replace FUNCTION s2_token_from_latlng(lat double precision, lng double precision) RETURNS text
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId.from_lat_lng(s2sphere.LatLng.from_degrees(lat, lng))
+  return id.to_token()
+$f$ LANGUAGE plpython3u IMMUTABLE;
+COMMENT ON function s2_token_from_latlng IS 'Get the string token for a cellid from the latitude and longitude';
+
+CREATE or replace FUNCTION s2_token_from_cellid(cellid bigint) RETURNS text
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  return id.to_token()
+$f$ LANGUAGE plpython3u IMMUTABLE;
+COMMENT ON function s2_token_from_cellid IS 'Get the string token for a cellid from the raw id';
+
+CREATE or replace FUNCTION s2_latlng_from_cellid(cellid bigint, OUT lat double precision, OUT lng double precision)
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  latlng = id.to_lat_lng()
+  return (latlng.lat().degrees, latlng.lng().degrees)
+$f$ LANGUAGE plpython3u IMMUTABLE;
+COMMENT ON function s2_latlng_from_cellid IS 'Get the latitude and longitude as degrees from a cellid';
+
+CREATE or replace FUNCTION s2_latlng_from_token(token text, OUT lat double precision, OUT lng double precision)
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  latlng = id.to_lat_lng()
+  return (latlng.lat().degrees, latlng.lng().degrees)
+$f$ LANGUAGE plpython3u IMMUTABLE;
+COMMENT ON function s2_latlng_from_token IS 'Get the latitude and longitude as degrees from a token';
+
+
+--
+-- CellId based functions
+--
+
+-- Return whether this is a valid s2 cellid.
+CREATE or replace FUNCTION s2_cellid_is_valid(cellid bigint) RETURNS boolean
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  return id.is_valid()
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+CREATE or replace FUNCTION s2_cellid_is_leaf(cellid bigint) RETURNS boolean
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  return id.is_leaf()
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+-- Return the level of the cellid
+CREATE or replace FUNCTION s2_cellid_level(cellid bigint) RETURNS int
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  return id.level()
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+-- Return a cell id at a parent level for the passed in cell id
+CREATE or replace FUNCTION s2_cellid_parent(cellid bigint, parent_level int) RETURNS bigint
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  newid = id.parent(parent_level)
+  return int.from_bytes(newid.id().to_bytes(8, 'big', signed=False), 'big', signed=True)
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+CREATE or replace FUNCTION s2_cellid_children(cellid bigint) RETURNS SETOF bigint
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  if not id.is_leaf():
+    for child in id.children():
+      yield int.from_bytes(child.id().to_bytes(8, 'big', signed=False), 'big', signed=True)
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+CREATE or replace FUNCTION s2_cellid_edge_neighbors(cellid bigint) RETURNS SETOF bigint
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId(int.from_bytes(cellid.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  for neighbor in id.get_edge_neighbors():
+    yield int.from_bytes(neighbor.id().to_bytes(8, 'big', signed=False), 'big', signed=True)
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+CREATE or replace FUNCTION s2_cellid_contains(a bigint, b bigint) RETURNS boolean
+AS $f$
+  import s2sphere
+  id_a = s2sphere.CellId(int.from_bytes(a.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  id_b = s2sphere.CellId(int.from_bytes(b.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  return id_a.contains(id_b)
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+CREATE or replace FUNCTION s2_cellid_intersects(a bigint, b bigint) RETURNS boolean
+AS $f$
+  import s2sphere
+  id_a = s2sphere.CellId(int.from_bytes(a.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  id_b = s2sphere.CellId(int.from_bytes(b.to_bytes(8, 'big', signed=True), 'big', signed=False))
+  return id_a.intersects(id_b)
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+CREATE or replace FUNCTION s2_cellid_exact_area(id0 bigint) RETURNS float8
+AS $f$
+  import s2sphere
+  return s2sphere.Cell( s2sphere.CellId(id0) ).exact_area()
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+
+--
+-- Token based functions
+--
+
+-- Return whether this is a valid s2 cellid.
+CREATE or replace FUNCTION s2_token_is_valid(token text) RETURNS boolean
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  return id.is_valid()
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+CREATE or replace FUNCTION s2_token_is_leaf(token text) RETURNS boolean
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  return id.is_leaf()
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+-- Return the level of the cellid
+CREATE or replace FUNCTION s2_token_level(token text) RETURNS int
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  return id.level()
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+-- Return a cell id at a parent level for the passed in cell id
+CREATE or replace FUNCTION s2_token_parent(token text, parent_level int) RETURNS text
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  newid = id.parent(parent_level)
+  return newid.to_token()
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+CREATE or replace FUNCTION s2_token_children(token text) RETURNS SETOF text
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  if not id.is_leaf():
+    for child in id.children():
+      yield child.to_token()
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+CREATE or replace FUNCTION s2_token_edge_neighbors(token text) RETURNS SETOF text
+AS $f$
+  import s2sphere
+  id = s2sphere.CellId.from_token(token)
+  for neighbor in id.get_edge_neighbors():
+    yield neighbor.to_token()
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+CREATE or replace FUNCTION s2_token_contains(a text, b text) RETURNS boolean
+AS $f$
+  import s2sphere
+  id_a = s2sphere.CellId.from_token(a)
+  id_b = s2sphere.CellId.from_token(b)
+  return id_a.contains(id_b)
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+CREATE or replace FUNCTION s2_token_intersects(a text, b text) RETURNS boolean
+AS $f$
+  import s2sphere
+  id_a = s2sphere.CellId.from_token(a)
+  id_b = s2sphere.CellId.from_token(b)
+  return id_a.intersects(id_b)
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+-- new, from 2019 changes:
+
+CREATE or replace FUNCTION s2_token_exact_area(token text) RETURNS float8 AS $f$
+  import s2sphere
+  return s2sphere.Cell( s2sphere.CellId.from_token(token) ).exact_area()
+$f$ LANGUAGE plpython3u IMMUTABLE;
+
+CREATE or replace FUNCTION s2_token_get_vertex(token text, k int,  OUT r float[])
+AS $f$
+  import s2sphere
+  point = s2sphere.Cell( s2sphere.CellId.from_token(token) ).get_vertex(k)
+  return ( point[0],  point[1],  point[2])
+$f$ LANGUAGE plpython3u IMMUTABLE;
+-- but ideal is GeoJSON, see clues at https://github.com/google/s2-geometry-library-java/issues/5
+
+--
+-- s2_token wrap functions:
+
+CREATE or replace FUNCTION s2_token_edge_neighbors_array(token text) RETURNS text[]
+AS $wrap$
+  SELECT array_agg(x) FROM s2_token_edge_neighbors(token) t(x)
+$wrap$ LANGUAGE SQL IMMUTABLE;
+
+CREATE or replace FUNCTION s2_token_children_array(token text) RETURNS text[]
+AS $wrap$
+  SELECT array_agg(x) FROM s2_token_children(token) t(x)
+$wrap$ LANGUAGE SQL IMMUTABLE;
